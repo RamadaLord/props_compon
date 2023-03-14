82bc9985b2797be7219c30c913169b135da16d2e
Ramdalord <petersongeraldo94@gmail.com>
2023-03-14 16:12:45 -0300

    TestesJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Equipe from './components/equipe'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Aula de componentes React.</h1>
+      <Equipe nome='Peterson' cargo= 'Garoto De Programa java' />
+      <Equipe nome='Lucas' cargo= 'Garoto De Programa Html'/>
+      <Equipe nome='Gabriel' cargo= 'Garoto De Programa Css' />
+      <Equipe nome='Emilio' cargo= 'Garoto De Programa JS'/>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/equipe/index.js
@@ -0,0 +1,12 @@
+function Equipe(props) {
+    return (
+        <div>
+            <hr />
+            <h1>G4C Technology/Colaborador:</h1>
+            <h2> Nome: {props.nome}</h2>
+                <h3>Cargo: {props.cargo}</h3>
+            
+        </div>
+    )
+}
+export default Equipe
